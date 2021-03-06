e03819a0cd321875edecf49538ba31793102b9be
erik.masny <erik.masny@gmail.com>
2021-03-06 13:33:55 +0100

    Installed jquery and added bootstrap to footer

--- footer.php
+++ footer.php
@@ -1,4 +1,15 @@
+<footer id="app-footer" class="bg-dark text-light">
 
+    <div class="container text-center py-3">
+        &copy; 2021 Erik Masny
+    </div>
+
+
+</footer>
+
+
+<script src="node_modules\jquery\dist\jquery.min.js"></script>
+<script src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
 
 </body>
 </html>
