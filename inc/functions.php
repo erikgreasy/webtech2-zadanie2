44448de13cd1c6e7c8829be70596b0ae0cf326d4
erik.masny <erik.masny@gmail.com>
2021-03-10 23:14:40 +0100

    Removed unnecessary functions. Added dd and view functions

--- inc/functions.php
+++ inc/functions.php
@@ -1,46 +1,22 @@
 <?php
 
 
-define( "BASE_URL", "http://localhost/webtech/zadanie2" );
-
-
 /**
- * Get Segments
- * 
- * From an url like http://example.com/edit/5
- * creates an array of URI segments [edit, 5]
- * 
- * @return array
+ * Show template, assign passed variables and die.
  */
-function get_segments() {
-
-    $current_url = 'http' .
-        ( isset( $_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on'  ? 's://' : '://' ) .
-        $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ;
-    
-
-    $path = str_replace( BASE_URL, '', $current_url );
-    $path = parse_url( $path, PHP_URL_PATH );
-
-
-    $segments = explode( '/', trim( $path, '/' ) );
-    
-
-    return $segments;
+function view( $template_file, $var_arr = [] ) {
+    extract($var_arr);
+    include_once 'views/' . $template_file;
+    exit;
 }
 
 
 /**
- * Segment
- * 
- * Returns the index-th URI segment
- * 
- * @param $index
- * @return string or false
+ * Dump n die. Prints out variable and then die like a real hero.
  */
-function segment( $index ) {
-    $segments = get_segments();
-
-    return isset( $segments[ $index-1 ] ) ? $segments[ $index-1 ] : false;
-    
+function dd( $var ) {
+    echo '<pre>';
+    var_dump($var);
+    echo '</pre>';
+    die();
 }
