c356586e6787caa42c6d4615b4fe7c35a0fcdc4e
erik.masny <erik.masny@gmail.com>
2021-03-10 23:13:28 +0100

    Added config.php

---
+++ inc/config.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+require_once 'functions.php';
+require_once 'helper-router-functions.php';
+
+define( "BASE_URL", "http://localhost/webtech/zadanie2" );
