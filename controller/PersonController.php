e6122c1b31ae41dd45e8213d108f240a7a4d580d
erik.masny <erik.masny@gmail.com>
2021-03-10 23:15:08 +0100

    Added PersonController functions

--- controller/PersonController.php
+++ controller/PersonController.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once 'model/Person.php';
-
 require_once 'inc/Database.php';
 
 class PersonController {
@@ -14,10 +13,68 @@ class PersonController {
     }
 
     public function index() {
-        $sql = $this->conn->query( 'SELECT * FROM PERSONS' );
-        $sql->setFetchMode(PDO::FETCH_CLASS, 'Person');
+        $query = "SELECT persons.id, CONCAT(persons.name, ' ',persons.surname) as name, olympic_games.year, olympic_games.city, olympic_games.type, standings.discipline 
+        FROM persons 
+        LEFT JOIN standings ON persons.id = standings.person_id
+        LEFT JOIN olympic_games ON olympic_games.id = standings.games_id";
+
+        if( isset( $_GET['orderby'] ) && $_GET['orderby'] != '' ) {
+            $orderby = $_GET['orderby'];
+    
+            switch($orderby) {
+                case 'surname':
+                    $query .= ' ORDER BY persons.surname';
+                    break;
+                case 'year':
+                    $query .= ' ORDER BY olympic_games.year';
+                    break;
+                default:
+                    $query .= ' ORDER BY persons.id';
+                    break;
+            }
+    
+            if( isset( $_GET['type_order'] ) ) {
+                $query .= ', olympic_games.type';
+            }
+
+        } else {
+            if( isset( $_GET['type_order'] ) ) {
+                $query .= ' ORDER BY olympic_games.type';
+            }
+        }
+    
+        
+        $sql = $this->conn->query( $query );
+        $sql->setFetchMode(PDO::FETCH_OBJ);
         $persons = $sql->fetchAll();
 
-        return $persons;
+        return view( 'person.index.php', [
+            'persons'   => $persons
+        ] );
+    }
+
+    public function edit($id) {
+        $sql = $this->conn->prepare( 'SELECT * FROM persons WHERE id = :id' );
+        $sql->execute([
+            'id'    => $id,
+        ]);
+        $person = $sql->fetch(PDO::FETCH_OBJ);
+
+        return view( 'person.edit.php', [
+            'person'    => $person
+        ] );
+
+    }
+
+    public function show($id) {
+        $sth = $this->conn->prepare("SELECT * FROM persons WHERE id = :id");
+        $sth->execute( [
+            'id'    => $id
+        ] );
+        $person = $sth->fetchAll(PDO::FETCH_CLASS, "Person")[0];
+
+        return view( 'person.php', [
+            'person'    => $person,
+        ] );
     }
 }
