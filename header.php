9403abe958c68afd0854f4c01c38b94dade3db83
erik.masny <erik.masny@gmail.com>
2021-03-11 08:57:49 +0100

    Changed up header links

--- header.php
+++ header.php
@@ -13,14 +13,17 @@
 
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="<?= BASE_URL ?>">Slovenskí olympionici</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="<?= BASE_URL ?>">Home <span class="sr-only">(current)</span></a>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= BASE_URL ?>">Všetci športovci <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= BASE_URL ?>/persons/create">Pridať nového</a>
                 </li>
             
             </ul>
