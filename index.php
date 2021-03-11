5c9622f971bdb47df7b12f1af476a52d73edd308
erik.masny <erik.masny@gmail.com>
2021-03-11 08:54:08 +0100

    Added 404.php and routing

--- index.php
+++ index.php
@@ -4,17 +4,25 @@
     require_once 'inc/functions.php';
     require_once 'controller/PersonController.php';
     
+    use Pecee\Http\Request;
     use Pecee\SimpleRouter\SimpleRouter;
 
     SimpleRouter::get('/webtech/zadanie2/', 'PersonController@index');
     
     SimpleRouter::get('/webtech/zadanie2/persons/{id}/edit', 'PersonController@edit');
     SimpleRouter::post('/webtech/zadanie2/persons/{id}/edit', 'PersonController@update');
+    SimpleRouter::delete('/webtech/zadanie2/persons/{id}/delete', 'PersonController@delete');
+
 
     SimpleRouter::get('/webtech/zadanie2/persons/create', 'PersonController@create');
     SimpleRouter::post('/webtech/zadanie2/persons/create', 'PersonController@store');
 
     SimpleRouter::get('/webtech/zadanie2/persons/{id}', 'PersonController@show');
-    
+
+    SimpleRouter::error(function(Request $request, \Exception $exception) {
+        
+        return view('404.php');
+        
+    });
 
     SimpleRouter::start();
---
+++ views/404.php
@@ -0,0 +1,12 @@
+
+<?php include 'header.php' ?>
+
+<main id="not-found">
+    <div class="container text-center">
+        <h1>404 :(</h1>
+        <p>Stratil si sa? Poď späť na <a href="<?= BASE_URL ?>">domovskú stránku</a></p>
+    </div>
+    
+</main>
+
+<?php include 'footer.php' ?>
