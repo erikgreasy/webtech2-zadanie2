de49c1beaea64000b18bccb74d77b788684b2431
erik.masny <erik.masny@gmail.com>
2021-03-13 18:41:53 +0100

    Added keeping of input values on error redirect back on standing.create

--- views/standing.create.php
+++ views/standing.create.php
@@ -24,7 +24,7 @@
                 <label for="person">Športovec:</label>
                 <select name="person" id="person" class="form-control">
                     <?php  foreach( $persons as $person ): ?>
-                        <option value="<?= $person->getId() ?>"><?= $person->getFullName() ?></option>
+                        <option value="<?= $person->getId() ?>" <?= isset($_POST['person']) && $_POST['person'] == $person->getId() ? 'selected' : '' ?>><?= $person->getFullName() ?></option>
                     <?php endforeach; ?>
                 </select>
             </div>
@@ -33,20 +33,20 @@
                 <label for="olympic_games">OH:</label>
                 <select name="olympic_games" id="olympic_games" class="form-control">
                     <?php  foreach( $olympic_games as $game ): ?>
-                        <option value="<?= $game->id ?>"><?= $game->name ?></option>
+                        <option value="<?= $game->id ?>" <?= isset($_POST['olympic_games']) && $_POST['olympic_games'] == $game->id ? 'selected' : '' ?>><?= $game->name ?></option>
                     <?php endforeach; ?>
                 </select>
             </div>
 
             <div class="form-group">
                 <label for="standing">Umiestnenie:</label>
-                <input type="number" name="standing" id="standing" class="form-control" min="1" max="50">
+                <input type="number" name="standing" id="standing" class="form-control" min="1" max="50" value="<?= isset( $_POST['standing'] ) ? $_POST['standing'] : '' ?>">
             </div>
 
 
             <div class="form-group">
                 <label for="discipline">Disciplína:</label>
-                <input type="text" name="discipline" id="discipline" class="form-control">
+                <input type="text" name="discipline" id="discipline" class="form-control" value="<?= isset( $_POST['discipline'] ) ? $_POST['discipline'] : '' ?>">
             </div>
 
 
