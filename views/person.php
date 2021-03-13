544b676aa90fe444d9679082e0d9dc59eab35584
erik.masny <erik.masny@gmail.com>
2021-03-13 17:50:30 +0100

    Changed person show view

--- views/person.php
+++ views/person.php
@@ -3,26 +3,58 @@
 
 <main>
     <div class="container"> 
-        <h1><?= $person->getFullName() ?></h1>
-        <a href="<?= BASE_URL ?>/persons/<?= $person->getId() ?>/edit">Edit</a>
-        <form action="<?= BASE_URL ?>/persons/<?= $person->getId() ?>/delete" method="POST">
-            <input type="hidden" name="_method" value="delete">
-            <button type="submit">Delete</button>
-        </form>
-
-        <h2>Narodenie</h2>
-        *
-        <?= $person->getBirthDay() ?>
-        <?= $person->getBirthPlace() ?>
-        <?= $person->getBirthCountry() ?>
         
-        <h2>Úmrtie</h2>
-        +
-        <?= $person->getDeathDay() ?>
-        <?= $person->getDeathPlace() ?>
-        <?= $person->getDeathCountry() ?>
+        <div class="jumbotron">
+            <h1 class="display-4"><?= $person->getFullName() ?></h1>
 
-    </div>
+            <hr>
+            <div class="row mt-4">
+            
+                <div class="col-lg-6">
+                    <h2>Narodenie</h2>
+                    <?= $person->getBirthDay() ?>
+                    <?= $person->getBirthPlace() ?>
+                    <?= $person->getBirthCountry() ?>
+                    
+                </div>
+                <div class="col-lg-6">
+                    <h2>Úmrtie</h2>
+                    <?= $person->getDeathDay() ?>
+                    <?= $person->getDeathPlace() ?>
+                    <?= $person->getDeathCountry() ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="my-5">
+            <h3>Tabuľka umiestnení</h3>
+            <table class="table">
+                <tr>
+                    <th>Umiestnenie</th>
+                    <th>Rok</th>
+                    <th>Miesto konania</th>
+                    <th>Typ OH</th>
+                    <th>Disciplína</th>
+                </tr>
+                
+                <?php foreach($standings as $standing): ?>
+
+                    <tr>
+                        <td><?= $standing->placing ?></td>
+                        <td><?= $standing->year ?></td>
+                        <td><?= $standing->city ?></td>
+                        <td><?= $standing->type ?></td>
+                        <td><?= $standing->discipline ?></td>
+
+
+                    </tr>
+
+                <?php endforeach; ?>
+            </table>
+        </div>
+
+    </div> <!-- END CONTAINER -->
+
 </main>
 
 
