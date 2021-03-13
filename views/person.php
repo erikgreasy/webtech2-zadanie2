2e7ae34660d60d390cb6f55cfa649c2fda302774
erik.masny <erik.masny@gmail.com>
2021-03-13 16:29:28 +0100

    Outputing person data on person detail

--- views/person.php
+++ views/person.php
@@ -4,6 +4,24 @@
 <main>
     <div class="container"> 
         <h1><?= $person->getFullName() ?></h1>
+        <a href="<?= BASE_URL ?>/persons/<?= $person->getId() ?>/edit">Edit</a>
+        <form action="<?= BASE_URL ?>/persons/<?= $person->getId() ?>/delete" method="POST">
+            <input type="hidden" name="_method" value="delete">
+            <button type="submit">Delete</button>
+        </form>
+
+        <h2>Narodenie</h2>
+        *
+        <?= $person->getBirthDay() ?>
+        <?= $person->getBirthPlace() ?>
+        <?= $person->getBirthCountry() ?>
+        
+        <h2>Úmrtie</h2>
+        +
+        <?= $person->getDeathDay() ?>
+        <?= $person->getDeathPlace() ?>
+        <?= $person->getDeathCountry() ?>
+
     </div>
 </main>
 
